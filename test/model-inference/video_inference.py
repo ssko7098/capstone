6934917d21c1e60f616e41711ab2d9856239133e
dkha5410 <dkha5410@uni.sydney.edu.au>
2024-10-22 21:16:09 +1100

    Pylint on video_inference.py

--- test/model-inference/video_inference.py
+++ test/model-inference/video_inference.py
@@ -1,20 +1,23 @@
+"""
+This script runs inference on a video using a YOLO model and saves the annotated output.
+"""
+
 from ultralytics import YOLO
-import matplotlib.pyplot as plt
 import cv2
 
-## choose the model you want to run inference on
+# Choose the model you want to run inference on
 model = YOLO('best.pt')
 
-video_path = 'test.mp4'
-cap = cv2.VideoCapture(video_path)
+VIDEO_PATH = 'test.mp4'
+cap = cv2.VideoCapture(VIDEO_PATH)  # pylint: disable=no-member
 
 # Define video codec and create VideoWriter object
-output_video_path = 'out.mp4'
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-fps = cap.get(cv2.CAP_PROP_FPS)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+OUTPUT_VIDEO_PATH = 'out.mp4'
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # pylint: disable=no-member
+fps = cap.get(cv2.CAP_PROP_FPS)  # pylint: disable=no-member
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # pylint: disable=no-member
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # pylint: disable=no-member
+out = cv2.VideoWriter(OUTPUT_VIDEO_PATH, fourcc, fps, (width, height))  # pylint: disable=no-member
 
 # Process each frame of the video
 while cap.isOpened():
@@ -22,23 +25,23 @@ while cap.isOpened():
     if not ret:
         break
 
-    # Run inference using the first model
+    # Run inference using the model
     results = model(frame)
 
-    # Annotate the frame with detection results from the first model
+    # Annotate the frame with detection results from the model
     annotated_frame = results[0].plot()
 
-    # Write the final frame with both models' annotations to the output video
+    # Write the final frame with annotations to the output video
     out.write(annotated_frame)
 
     # Optional: Display the frame during processing
-    cv2.imshow('YOLOv8 Multiple Models Inference', annotated_frame)
+    cv2.imshow('YOLOv8 Inference', annotated_frame)
 
     # Press 'q' to exit the video display
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # pylint: disable=no-member
         break
 
 # Release the video capture, video writer, and close any OpenCV windows
 cap.release()
 out.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()  # pylint: disable=no-member
